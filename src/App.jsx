4eb68675df4c73f289a766ad8946b09b407f4841
Tejalwakale <tejalwakale0812@gmail.com>
2026-08-14 16:46:34 +0530

    Implement createBrowserRouter routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
 import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 
-const Home = lazy(() => import("./pages/Home"));
-const ProductDetail = lazy(() => import("./components/ProductDetail"));
-const Cart = lazy(() => import("./components/Cart"));
-const Checkout = lazy(() => import("./pages/Checkout"));
-const NotFound = lazy(() => import("./components/NotFound"));
-
-function App() {
+const HeaderLayout = () => {
   return (
     <>
       <Header />
 
       <Suspense fallback={<p>Loading page...</p>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route
-            path="/product/:id"
-            element={<ProductDetail />}
-          />
-
-          <Route
-            path="/cart"
-            element={<Cart />}
-          />
-
-          <Route
-            path="/checkout"
-            element={<Checkout />}
-          />
-
-          <Route
-            path="*"
-            element={<NotFound />}
-          />
-        </Routes>
-      </Suspense>
+        <Outlet />
+      </Suspense> 
     </>
   );
-}
+};
 
-export default App;
+export default HeaderLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,52 @@
-import { StrictMode } from 'react';
+import { StrictMode, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import './index.css';
-import App from './App.jsx';
 import store from "./redux/store.js"
+
+const Home = lazy(() => import("./pages/Home"));
+const ProductDetail = lazy(() => import("./components/ProductDetail"));
+const Cart = lazy(() => import("./components/Cart"));
+const Checkout = lazy(() => import("./pages/Checkout"));
+const NotFound = lazy(() => import("./components/NotFound"));
+const App = lazy(() => import("./App.jsx"));
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+    },
+    {
+      path: "product/:id",
+      element: <ProductDetail />,
+    },
+    {
+      path: "cart",
+      element: <Cart />,
+    },
+    {
+      path: "checkout",
+      element: <Checkout />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
+  },
+]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
